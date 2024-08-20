c26a1b67ed542a26672b860bd1e5c5913f5b674c
Sanjay Ginde <sanjayginde@gmail.com>
2024-08-19 21:32:16 -0400

    More complete lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -22,7 +22,7 @@ impl Lexer {
     }
 
     pub fn read_char(&mut self) {
-        if (self.read_position > self.input.len()) {
+        if self.read_position > self.input.len() {
             self.ch = None;
         } else {
             self.ch = self.input.chars().nth(self.read_position);
@@ -50,7 +50,39 @@ impl Lexer {
             '+' => Token::from_char(Plus, ch),
             '{' => Token::from_char(LeftBrace, ch),
             '}' => Token::from_char(RightBrace, ch),
-            _ => Token::from_char(Illegal, ch),
+            _ => {
+                if ch.is_whitespace() {
+                    while self.ch.is_some() && self.ch.unwrap().is_whitespace() {
+                        self.read_char();
+                    }
+
+                    return self.next_token();
+                } else if ch.is_alphabetic() {
+                    let mut literal = String::new();
+                    literal.push(ch);
+                    while self.ch.is_some() && self.ch.unwrap().is_alphabetic() {
+                        literal.push(self.ch.unwrap());
+                        self.read_char();
+                    }
+
+                    match literal.as_str() {
+                        "let" => Token::from_str(Let, &literal),
+                        "fn" => Token::from_str(Function, &literal),
+                        _ => Token::from_str(Ident, &literal),
+                    }
+                } else if ch.is_digit(10) {
+                    let mut literal = String::new();
+                    literal.push(ch);
+                    while self.ch.is_some() && self.ch.unwrap().is_digit(10) {
+                        literal.push(self.ch.unwrap());
+                        self.read_char();
+                    }
+
+                    Token::from_str(Int, &literal)
+                } else {
+                    Token::from_char(Illegal, ch)
+                }
+            }
         }
     }
 }
@@ -63,9 +95,8 @@ mod test {
     use super::Lexer;
 
     #[test]
-    fn test_solve() {
+    fn test_next_token_simple() {
         let input = "=+(){},;";
-
         let mut lexer = Lexer::new(input.to_string());
 
         let expected = vec![
@@ -82,6 +113,67 @@ mod test {
 
         for token in expected {
             assert_eq!(lexer.next_token(), token);
+        }
+    }
+
+    #[test]
+    fn test_next_token_complex() {
+        let input = r#"
+let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+  x + y;
+};
+
+let result = add(five, ten);
+"#;
+        let mut lexer = Lexer::new(input.to_string());
+
+        let expected = vec![
+            Token::from_str(Let, "let"),
+            Token::from_str(Ident, "five"),
+            Token::from_str(Assign, "="),
+            Token::from_str(Int, "5"),
+            Token::from_str(Semicolon, ";"),
+            Token::from_str(Let, "let"),
+            Token::from_str(Ident, "ten"),
+            Token::from_str(Assign, "="),
+            Token::from_str(Int, "10"),
+            Token::from_str(Semicolon, ";"),
+            Token::from_str(Let, "let"),
+            Token::from_str(Ident, "add"),
+            Token::from_str(Assign, "="),
+            Token::from_str(Function, "fn"),
+            Token::from_str(LeftParen, "("),
+            Token::from_str(Ident, "x"),
+            Token::from_str(Comma, ","),
+            Token::from_str(Ident, "y"),
+            Token::from_str(RightParen, ")"),
+            Token::from_str(LeftBrace, "{"),
+            Token::from_str(Ident, "x"),
+            Token::from_str(Plus, "+"),
+            Token::from_str(Ident, "y"),
+            Token::from_str(Semicolon, ";"),
+            Token::from_str(RightBrace, "}"),
+            Token::from_str(Semicolon, ";"),
+            Token::from_str(Let, "let"),
+            Token::from_str(Ident, "result"),
+            Token::from_str(Assign, "="),
+            Token::from_str(Ident, "add"),
+            Token::from_str(LeftParen, "("),
+            Token::from_str(Ident, "five"),
+            Token::from_str(Comma, ","),
+            Token::from_str(Ident, "ten"),
+            Token::from_str(RightParen, ")"),
+            Token::from_str(Semicolon, ";"),
+            Token::from_str(Eof, ""),
+        ];
+
+        for token in expected {
+            let next_token = lexer.next_token();
+            println!("{:?}", token);
+            assert_eq!(next_token, token);
         }
     }
 }
